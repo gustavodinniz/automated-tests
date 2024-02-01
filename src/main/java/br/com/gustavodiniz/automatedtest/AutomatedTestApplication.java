cea526734a7ce2d964e3475c662e1c9a0221e8de
gustavodiniz <gustavodinniz.dev@gmail.com>
2024-02-01 20:21:29 -0300

    feat: structure project

---
+++ src/main/java/br/com/gustavodiniz/automatedtest/AutomatedTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavodiniz.automatedtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gustavodiniz/automatedtest/AutomatedTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavodiniz.automatedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
