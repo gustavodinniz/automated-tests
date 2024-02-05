ae357ddbf8cef4474f8caab3c391b96e85281b72
gustavodiniz <gustavodinniz.dev@gmail.com>
2024-02-05 19:48:41 -0300

    feat: implementing BDD style for all tests

--- src/test/java/br/com/gustavodiniz/automatedtest/math/SimpleMathTest.java
+++ src/test/java/br/com/gustavodiniz/automatedtest/math/SimpleMathTest.java
@@ -11,97 +11,171 @@ class SimpleMathTest {
     //[System Under Test][Condition or Stage Change][Expected Result] sumWhenSixDotTwoIsAddedByTwoShouldReturnEightDotTwo
     //[System Under Test][Expected Result][Condition or Stage Change] sumShouldReturnEightDotTwoWhenSixDotTwoIsAddedByTwo
 
+    private final SimpleMath math = new SimpleMath();
+
+    private double firstNumber;
+
+    private double secondNumber;
+
+    private double expectedNumber;
+
+    private Double actual;
+
     @Test
     @DisplayName("Sum when 6.2 is added by 2 should return 8.2")
     void sumShouldReturnEightDotTwoWhenSixDotTwoIsAddedByTwo() {
-        SimpleMath math = new SimpleMath();
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
-        Double actual = math.sum(firstNumber, secondNumber);
-        double expected = 8.2D;
-
-        assertEquals(expected, actual, "The sum method should return the sum of two numbers");
-        assertNotEquals(0, actual);
-        assertNotNull(actual);
+        givenFirstNumber();
+        givenSecondNumber();
+        givenSumExpectedNumber();
+        whenMathSumCalled();
+        thenExpectMathSumReturnsExpectedNumber();
+        thenExpectReturnsNonZero();
+        thenExpectReturnsNotNull();
     }
+
 
     @Test
     @DisplayName("Subtraction when 6.2 is subtracted by 2 should return 4.2")
     void subtractionShouldReturnFourDotTwoWhenSixDotTwoIsSubtractedByTwo() {
-        SimpleMath math = new SimpleMath();
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
-        Double actual = math.subtraction(firstNumber, secondNumber);
-        double expected = 4.2D;
-
-        assertEquals(expected, actual, "The subtraction method should return the subtraction of two numbers");
-        assertNotEquals(0, actual);
-        assertNotNull(actual);
+        givenFirstNumber();
+        givenSecondNumber();
+        givenSubtractExpectNumber();
+        whenMathSubtractionCalled();
+        thenExpectReturnsExpectedNumber();
+        thenExpectReturnsNonZero();
+        thenExpectReturnsNotNull();
     }
+
 
     @Test
     @DisplayName("Multiplication when 6.2 is multiplied by 2 should return 12.4")
     void multiplicationShouldReturnTwelveDotFourWhenSixDotTwoIsMultipliedByTwo() {
-        SimpleMath math = new SimpleMath();
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
-        Double actual = math.multiplication(firstNumber, secondNumber);
-        double expected = 12.4D;
-
-        assertEquals(expected, actual, "The multiplication method should return the multiplication of two numbers");
-        assertNotEquals(0, actual);
-        assertNotNull(actual);
+        givenFirstNumber();
+        givenSecondNumber();
+        givenMultiplicationExpectedNumber();
+        whenMathMultiplicationCalled();
+        thenExpectReturnsExpectedNumber();
+        thenExpectReturnsNonZero();
+        thenExpectReturnsNotNull();
     }
+
 
     @Test
     @DisplayName("Division when 6.2 is divided by 2 should return 3.1")
     void divisionShouldReturnThreeDotOneWhenSixDotTwoIsDividedByTwo() {
-        SimpleMath math = new SimpleMath();
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
-        Double actual = math.division(firstNumber, secondNumber);
-        double expected = 3.1D;
-
-        assertEquals(expected, actual, "The division method should return the division of two numbers");
-        assertNotEquals(0, actual);
-        assertNotNull(actual);
+        givenFirstNumber();
+        givenSecondNumber();
+        whenMathDivisionCalled();
+        givenDivisionExpectedNumber();
+        thenExpectReturnsExpectedNumber();
+        thenExpectReturnsNonZero();
+        thenExpectReturnsNotNull();
     }
 
-    @Test
-    @DisplayName("Division when 6.2 is divided by 0 should return Infinity")
-    void divisionShouldReturnInfinityWhenFirstNumberIsDividedByZero() {
-        SimpleMath math = new SimpleMath();
-        double firstNumber = 6.2D;
-        double secondNumber = 0D;
-        Double actual = math.division(firstNumber, secondNumber);
-
-        assertEquals(Double.POSITIVE_INFINITY, actual, "The division method should return the division of two numbers");
-    }
 
     @Test
     @DisplayName("Mean when 6.2 and 2 is calculated should return 4.1")
     void meanShouldReturnFourDotOneWhenAverageOfSixDotTwoAndTwoIsCalculated() {
-        SimpleMath math = new SimpleMath();
-        double firstNumber = 6.2D;
-        double secondNumber = 2D;
-        Double actual = math.mean(firstNumber, secondNumber);
-        double expected = 4.1D;
-
-        assertEquals(expected, actual, "The mean method should return the mean of two numbers");
-        assertNotEquals(0, actual);
-        assertNotNull(actual);
+        givenFirstNumber();
+        givenSecondNumber();
+        whenMathMeanCalled();
+        givenMeanExpectedNumber();
+        thenExpectReturnsExpectedNumber();
+        thenExpectReturnsNonZero();
+        thenExpectReturnsNotNull();
     }
 
-    @Test
-    @DisplayName("Square root when 9 is calculated should return 3")
-    void squareRootShouldReturnThreeWhenSquareRootOfNineIsCalculated() {
-        SimpleMath math = new SimpleMath();
-        double number = 9D;
-        Double actual = math.squareRoot(number);
-        double expected = 3D;
 
-        assertEquals(expected, actual, "The squareRoot method should return the square root of a number");
+    @Test
+    @DisplayName("Square root when 6.2 is calculated should return 2.48997991928")
+    void squareRootShouldReturnThreeWhenSquareRootOfNineIsCalculated() {
+        givenFirstNumber();
+        givenSquareRootExpectedNumber();
+        whenMathSquareRootCalled();
+        thenExpectMathSumReturnsExpectedNumber();
+        thenExpectReturnsNonZero();
+        thenExpectReturnsNotNull();
+    }
+
+
+    // given
+
+    private void givenSquareRootExpectedNumber() {
+        expectedNumber = 2.4899799195977463D;
+    }
+
+
+    private void givenMultiplicationExpectedNumber() {
+        expectedNumber = 12.4D;
+    }
+
+    private void givenDivisionExpectedNumber() {
+        expectedNumber = 3.1D;
+    }
+
+    private void givenMeanExpectedNumber() {
+        expectedNumber = 4.1D;
+    }
+
+    private void givenSubtractExpectNumber() {
+        expectedNumber = 4.2D;
+    }
+
+    private void givenSumExpectedNumber() {
+        expectedNumber = 8.2D;
+    }
+
+    private void givenSecondNumber() {
+        secondNumber = 2D;
+    }
+
+    private void givenFirstNumber() {
+        firstNumber = 6.2D;
+    }
+
+
+    // when
+
+    private void whenMathSquareRootCalled() {
+        actual = math.squareRoot(firstNumber);
+    }
+
+    private void whenMathMultiplicationCalled() {
+        actual = math.multiplication(firstNumber, secondNumber);
+    }
+
+    private void whenMathDivisionCalled() {
+        actual = math.division(firstNumber, secondNumber);
+    }
+
+    private void whenMathMeanCalled() {
+        actual = math.mean(firstNumber, secondNumber);
+    }
+
+    private void whenMathSubtractionCalled() {
+        actual = math.subtraction(firstNumber, secondNumber);
+    }
+
+    private void whenMathSumCalled() {
+        actual = math.sum(firstNumber, secondNumber);
+    }
+
+
+    // then
+
+    private void thenExpectReturnsExpectedNumber() {
+        assertEquals(expectedNumber, actual, "The subtraction method should return the subtraction of two numbers");
+    }
+
+    private void thenExpectMathSumReturnsExpectedNumber() {
+        assertEquals(expectedNumber, actual, "The sum method should return the sum of two numbers");
+    }
+
+    private void thenExpectReturnsNonZero() {
         assertNotEquals(0, actual);
+    }
+
+    private void thenExpectReturnsNotNull() {
         assertNotNull(actual);
     }
 
